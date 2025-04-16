016e013697c6047bca2d61ce4bfbe108950783d0
carlaLopez <carla.lopezsanchez20@gmail.com>
2025-04-16 13:51:48 -0400

    created RAG system

--- knowledge_graphs/program.py
+++ knowledge_graphs/program.py
@@ -1,24 +1,65 @@
 import ollama
 import logging
+import chromadb
 
 
 dataset = []
+client = chromadb.Client()
 
-#read data set
+collection = client.create_collection(name="us-presidents-facts")
+
+#read data.txt and add it to dataset array 
 with open("data.txt", "r") as file:
     dataset = file.readlines()
     for i,line in enumerate(dataset):
         clean_text = line.replace("\n", "")
         dataset[i] = clean_text    
 
-##use ollama embedding model to create embeddings for data
-
+##create an embedding for each one of the facts
 for i,line in enumerate(dataset):
-    embedding = ollama.embed(
+    response = ollama.embed(
         model='mxbai-embed-large',
         input=line
-    )["embeddings"]
-    ##store to pgvector
+    )
+    embeddings = response["embeddings"]
+    collection.add(
+        documents=[line],
+        ids=[str(i)],
+        embeddings=embeddings
+    )
+
+#generate response
+
+def generate_model_response(prompt,data):
+    instruction_prompt = f"Use only the following pieces of context to answer the question: {data}"
+
+    output = ollama.chat(
+            model= 'llama3.2:1b',
+           messages=[
+            {'role': 'system', 'content': instruction_prompt},
+            {'role': 'user', 'content': prompt},
+    ],
+    stream=True
+    )
+    return output
 
 
-## get query of user, embed it and query it from pg vector
+while(True):
+
+    userInput = input("Ask a question \n")
+
+    ##create an embedding for the users prompt
+    inputEmbedding = ollama.embed(
+        model='mxbai-embed-large',
+        input = userInput
+    )
+
+    results = collection.query(
+        query_embeddings=inputEmbedding["embeddings"],
+        n_results=5
+    )
+    data = results['documents'][0][0]
+    output = generate_model_response(userInput,data)
+    
+    for r in output:
+        print(r.message.content + "\n")
